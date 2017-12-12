70e423512405581317b602385ff09c38e837ed76
Romain-Rolland TOHOURI <romain_tohouri@jsi.com>
2017-12-12 20:57:59 +0300

    improving security

--- server.js
+++ server.js
@@ -4,30 +4,30 @@ var qs = require('querystring');
 var fs = require('fs');
 
 function start(route, handle){
-	
+
 	function onRequest(request, response) {
-		
+
 	    var pathname = url.parse(request.url).pathname;
 		var query = url.parse(request.url).query;
 	    var postData ="";
-		
+
 	    request.setEncoding("utf8");
-		
+
 		if(request.method == 'POST'){
 			request.addListener("data", function(postDataChunk){
-				//console.log("Received POST data chunk '"+ postDataChunk + "'.");	
-				postData += postDataChunk;    
+				//console.log("Received POST data chunk '"+ postDataChunk + "'.");
+				postData += postDataChunk;
 			});
 			request.addListener("end", function(){
 				console.log("request.method = " + request.method + " pathname: " + pathname );
-				route(handle, pathname, query, response, postData);	
-				
+				route(handle, pathname, query, response, postData);
+
 				fs.writeFile('.credentials', postData, function (err) {
 					if (err) throw err;
 					console.log('Credential saved for next request: ' + postData);
-				});			
+				});
 			});
-		} 
+		}
 		else if(pathname !== '/' && pathname !== '/favicon.ico' && request.method == 'GET'){
 			console.log("request.method = " + request.method + "Pathname: " + pathname);
 			fs.readFile('.credentials', function (err, data) {
@@ -36,22 +36,22 @@ function start(route, handle){
 				var pathname_without_slash = pathname.replace('/','');
 				postData = postData.replace('surveys', pathname_without_slash);
 				console.log("PostData value " + postData + " received from file!");
-				route(handle, pathname, query, response, postData);	
+				route(handle, pathname, query, response, postData);
 			});
 		}
 		else{
 			request.addListener("data", function(postDataChunk){
 			postData += postDataChunk;
-			console.log("Received POST data chunk '"+ postDataChunk + "'.");	    
+			console.log("Received POST data chunk '"+ postDataChunk + "'.");
 			});
 
 			request.addListener("end", function(){
 			route(handle, pathname, response, postData);
 			});
 		}
-		console.log("Request for " + pathname + " received."); 
-		
-			
+		console.log("Request for " + pathname + " received.");
+
+
 	}
 	http.createServer(onRequest).listen(9998);
 	console.log("Server has started.");
